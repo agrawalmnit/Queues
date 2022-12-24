5d56a046e442a4f4c7527f60f1241d99461c7db7
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-24 16:16:29 +0530

    Add files via upload

---
+++ ConsoleApp36/ConsoleApp36/Class1.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp36
+//{
+//    public class Class1
+//    {
+//        // definations
+
+
+//        // queue allows inserting element at one end and removing from the other end 
+
+//        // queue allows null and duplicate values 
+
+//        // enqueue adds element at the end of the queue
+
+//        // dequeue removes element from the start of the queue
+
+//        // peek returns the element from the start of queue that is oldest
+
+//        // queue is used when we want to access the information in the same order as it is inserted
+
+//        // IsSynchronized creates a new synchronized wrapper aaround the queue
+
+//        // clear removes all the elements from the queue
+
+//        // 
+//    }
+//}
